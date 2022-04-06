06d420ccc904435517bfaec1944658a09caa8a0d
coder1973 <62132667+coder1973@users.noreply.github.com>
2022-04-06 19:42:26 +0200

    Update GitHubCalismasi.java

--- src/test/java/GitHub/GitHubCalismasi.java
+++ src/test/java/GitHub/GitHubCalismasi.java
@@ -5,5 +5,8 @@ public class GitHubCalismasi {
     public static void main(String[] args) {
 
         System.out.println("GitHub dersi Basladi!");
+        
+         System.out.println("GitHubda degisiklik yaptim");
+        
     }
 }
