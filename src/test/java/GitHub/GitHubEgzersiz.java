463499186dba715c68db570fcb7f9151edbc2cdd
coder1973 <bulentozturk2019@outlook.com>
2022-04-08 18:09:34 +0200

    update

--- src/test/java/GitHub/GitHubEgzersiz.java
+++ src/test/java/GitHub/GitHubEgzersiz.java
@@ -5,5 +5,6 @@ public class GitHubEgzersiz {
     public static void main(String[] args) {
 
         System.out.println("Yeni calisma");
+        System.out.println("yeniBranch calisma yapilyor");
     }
 }
